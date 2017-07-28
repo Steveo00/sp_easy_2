9bb45f48241206b8136d35ab83a6b70fdc4704cf
Steveo00 <srwillis@ozemail.com.au>
2017-07-28 14:23:14 +1000

    completed remaining exercises

--- sum_or_product_of_consecutive_integers.rb
+++ sum_or_product_of_consecutive_integers.rb
@@ -1,14 +1,22 @@
 def calculate_sum(int)
-  sum = 0
-  1.upto(int) { |num| sum += num }
-  sum
+  (1..int).inject(:+)  
 end
 
 def calculate_product(int)
-  product = 1
-  1.upto(int) { |num| product *= num }
-  product
+  (1..int).inject(:*) 
 end
+
+# def calculate_sum(int)
+#  sum = 0
+#  1.upto(int) { |num| sum += num }
+#  sum
+# end
+
+# def calculate_product(int)
+#   product = 1
+#   1.upto(int) { |num| product *= num }
+#   product
+# end
 
 puts ">> Please enter an integer greater than 0:"
 user_integer = gets.chomp.to_i
